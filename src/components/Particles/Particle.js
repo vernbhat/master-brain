a79095460dd8188c6c0c90c60febaf14d0d68f7d
Kaushik Shankar Bhat <kaushik.2003.ks@gmail.com>
2023-06-14 18:42:45 +0530

    after background

--- src/components/Particles/Particle.js
+++ src/components/Particles/Particle.js
@@ -140,19 +140,19 @@ const Particle = () => {
         //     // value: "#0d47a1",
         //   },
         // },
-        fpsLimit: 60,
+        fpsLimit: 120,
         interactivity: {
-          events: {
-            onClick: {
-              enable: true,
-              mode: "push",
-            },
-            onHover: {
-              enable: true,
-              mode: "repulse",
-            },
-            resize: true,
-          },
+        //   events: {
+        //     onClick: {
+        //       enable: true,
+        //       mode: "push",
+        //     },
+        //     onHover: {
+        //       enable: true,
+        //       mode: "repulse",
+        //     },
+        //     resize: true,
+        //   },
           modes: {
             push: {
               quantity: 4,
